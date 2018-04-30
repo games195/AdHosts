a7ab13bff552f07966ad986bc733c353dbb49f59
Unknown <games195dev@gmail.com>
2018-04-30 22:11:53 +0200

    Little update

--- AdHosts.py
+++ AdHosts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # Original script by ildoc @ https://github.com/ildoc/HostBlock
-# Copyright (C) 2018 games195. Licensed under MIT License.
+# Copyright (c) 2018 games195 <games195dev@gmail.com>. Licensed under MIT License.
 
 import urllib.request, datetime, os
 
@@ -12,6 +12,8 @@ List = []
 # Thanks to all maintainers of ad-hosts lists.
 Sources = [
     'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts',
+    'https://gist.githubusercontent.com/games195/4dced15b925a4cc28e40285b0c05ac31/raw/hosts.txt',
+    'https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt',
     'https://raw.githubusercontent.com/biroloter/Mobile-Ad-Hosts/master/hosts',
     'https://raw.githubusercontent.com/vokins/yhosts/master/hosts',
     'https://hosts-file.net/ad_servers.txt'
@@ -19,7 +21,7 @@ Sources = [
 
 for Link in Sources:
     try:
-        print('[+] Retrieving list from: %s' % Link)
+        print('[+] Retrieving list from: {}'.format(Link))
         r = urllib.request.urlopen(Link)
         Host = r.readlines()
         Host = [x.decode('utf-8') for x in Host]
@@ -27,15 +29,15 @@ for Link in Sources:
         Host = [z for z in Host if z != '' and z[0] != '#']
         Host = [h.split()[1] for h in Host if h.split()[0] in ['0.0.0.0', '127.0.0.1']]
         Host = [x for x in Host if x not in ['localhost', 'localhost.localdomain', 'locals']]
-        print('[+] %s blocked domains found.' % str(len(Host)))
+        print('[+] {} blocked domains found.'.format(str(len(Host))))
         r.close()
         List += Host
     except:
-        print('[-] ERROR: I can\'t retrieve the list from: %s' % Link)
+        print('[-] ERROR: I can\'t retrieve the list from: {}'.format(Link))
 
 print('[+] Removing duplicates and sorting...')
 List = sorted(list(set(List)))
-print('[+] Total domains count %s.' % str(len(List)))
+print('[+] Total domains count {}.'.format(str(len(List))))
 
 if not os.path.exists(os.path.dirname(File)):
     os.makedirs(os.path.dirname(File))
@@ -45,7 +47,7 @@ with open(File, 'w') as f:
     f.write('''
 # This file was generated thanks to various sources.
 # Repo: https://github.com/games195/AdHosts
-# Last updated: %s\n
-''' % datetime.datetime.now().strftime('%a, %d %b %y %X'))
+# Last updated: {}\n
+'''.format(datetime.datetime.now().strftime('%a, %d %b %y %X')))
     f.write('\n'.join('127.0.0.1 ' + url for url in List))
     print('[+] Done!')
--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 AdHosts is a python script to retrieve ad-hosts lists from various sources and merge its in one file updated every day!
 
 [`hosts.txt`](https://games195.github.io/AdHosts/hosts.txt) contains various server of adware, malware, crypto miner, gambling, fakenews and mobile ad.
+
 ## Usage
 
 For Android you can use [AdAway](https://f-droid.org/packages/org.adaway/) (for rooted devices) or [DNS66](https://f-droid.org/packages/org.jak_linux.dns66/) (for unrooted devices) and add [`hosts.txt`](https://games195.github.io/AdHosts/hosts.txt) to app's hosts list.
@@ -10,7 +11,7 @@ For Android you can use [AdAway](https://f-droid.org/packages/org.adaway/) (for 
 ## Credits
 
 * [ildoc](https://github.com/ildoc) for [HostBlock](https://github.com/ildoc/HostBlock) code base.
-* Thanks to all [maintainers](https://github.com/games195/AdHosts/blob/master/AdHosts.py#L10) of ad-hosts lists.
+* Thanks to all [maintainers](https://github.com/games195/AdHosts/blob/master/AdHosts.py#L12) of ad-hosts lists.
 
 ## Licensing
 
