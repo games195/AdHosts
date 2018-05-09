f7f1c183a524b144150f8048773163cb0c964b08
Unknown <games195dev@gmail.com>
2018-05-09 19:00:44 +0200

    Several updates to AdHosts.py and README.md
    - New hosts.
    - Add 'Features' section.
    - Simplified 'Usage' section.
    - Other minor fixes.

--- AdHosts.py
+++ AdHosts.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 # Original script by ildoc @ https://github.com/ildoc/HostBlock
 # Copyright (c) 2018 games195 <games195dev@gmail.com>. Licensed under MIT License.
@@ -6,48 +7,41 @@
 import urllib.request, datetime, os
 
 File = 'host/hosts.txt'
-
 List = []
-
 # Thanks to all maintainers of ad-hosts lists.
 Sources = [
-    'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts',
-    'https://gist.githubusercontent.com/games195/4dced15b925a4cc28e40285b0c05ac31/raw/hosts.txt',
-    'https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/hosts.txt',
-    'https://raw.githubusercontent.com/biroloter/Mobile-Ad-Hosts/master/hosts',
-    'https://raw.githubusercontent.com/vokins/yhosts/master/hosts',
-    'https://hosts-file.net/ad_servers.txt'
+	'https://raw.githubusercontent.com/AdroitAdorKhan/Energized/master/EnergizedHosts/EnergizedHosts',
+	'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts',
+	'https://gist.githubusercontent.com/games195/4dced15b925a4cc28e40285b0c05ac31/raw/hosts.txt',
+	'https://raw.githubusercontent.com/AdroitAdorKhan/Energized/master/Hosts/Energized',
+	'https://raw.githubusercontent.com/biroloter/Mobile-Ad-Hosts/master/hosts'
 ]
 
 for Link in Sources:
-    try:
-        print('[+] Retrieving list from: {}'.format(Link))
-        r = urllib.request.urlopen(Link)
-        Host = r.readlines()
-        Host = [x.decode('utf-8') for x in Host]
-        Host = [x.strip() for x in Host]
-        Host = [z for z in Host if z != '' and z[0] != '#']
-        Host = [h.split()[1] for h in Host if h.split()[0] in ['0.0.0.0', '127.0.0.1']]
-        Host = [x for x in Host if x not in ['localhost', 'localhost.localdomain', 'locals']]
-        print('[+] {} blocked domains found.'.format(str(len(Host))))
-        r.close()
-        List += Host
-    except:
-        print('[-] ERROR: I can\'t retrieve the list from: {}'.format(Link))
+	try:
+		print('[+] Retrieving list from: {}'.format(Link))
+		r = urllib.request.urlopen(Link)
+		Host = r.readlines()
+		Host = [x.decode('utf-8') for x in Host]
+		Host = [x.strip() for x in Host]
+		Host = [z for z in Host if z != '' and z[0] != '#']
+		Host = [h.split()[1] for h in Host if h.split()[0] in ['0.0.0.0', '127.0.0.1']]
+		Host = [x for x in Host if x not in ['localhost', 'localhost.localdomain', 'locals']]
+		print('[+] Found {} domains to block.'.format(str(len(Host))))
+		r.close()
+		List += Host
+	except:
+		print('[-] ERROR: I can\'t retrieve the list from: {}'.format(Link))
 
 print('[+] Removing duplicates and sorting...')
 List = sorted(list(set(List)))
 print('[+] Total domains count {}.'.format(str(len(List))))
 
 if not os.path.exists(os.path.dirname(File)):
-    os.makedirs(os.path.dirname(File))
+	os.makedirs(os.path.dirname(File))
 
 with open(File, 'w') as f:
-    print('[+] Writing to file...')
-    f.write('''
-# This file was generated thanks to various sources.
-# Repo: https://github.com/games195/AdHosts
-# Last updated: {}\n
-'''.format(datetime.datetime.now().strftime('%a, %d %b %y %X')))
-    f.write('\n'.join('127.0.0.1 ' + url for url in List))
-    print('[+] Done!')
+	print('[+] Writing to file...')
+	f.write('''\n# This file was generated thanks to various sources.\n# Repo: https://github.com/games195/AdHosts\n# Last updated: {}\n\n'''.format(datetime.datetime.now().strftime('%a, %d %b %y %X')))
+	f.write('\n'.join('127.0.0.1 ' + url for url in List))
+	print('[+] Done!')
--- README.md
+++ README.md
@@ -1,19 +1,28 @@
-# AdHosts
+# AdHosts [![Build Status](https://www.travis-ci.org/games195/AdHosts.svg?branch=master)](https://www.travis-ci.org/games195/AdHosts) ![license](https://img.shields.io/badge/License-MIT-blue.svg)
 
-AdHosts is a python script to retrieve ad-hosts lists from various sources and merge its in one file updated every day!
+AdHosts is a python script to retrieve ad-hosts lists from various sources and merge it's in one file updated every day!
 
-[`hosts.txt`](https://games195.github.io/AdHosts/hosts.txt) contains various server of adware, malware, crypto miner, gambling, fakenews and mobile ad.
+## Features
+
+- [x] Block all advertisements, malware, crypto miners, scams, statistics, trackers on both web browsing and applications.
+- [x] Reduces page loading times and data consumptions.
+- [x] Compatible with all devices, regardless of OS.
+- [x] Increases privacy.
 
 ## Usage
 
-For Android you can use [AdAway](https://f-droid.org/packages/org.adaway/) (for rooted devices) or [DNS66](https://f-droid.org/packages/org.jak_linux.dns66/) (for unrooted devices) and add [`hosts.txt`](https://games195.github.io/AdHosts/hosts.txt) to app's hosts list.
+For Android you can use:
+
+* [DNS66](https://f-droid.org/packages/org.jak_linux.dns66/) for unrooted devices.
+* [AdAway](https://f-droid.org/packages/org.adaway/) for rooted devices.
+
+Add [`https://games195.github.io/AdHosts/hosts.txt`](https://games195.github.io/AdHosts/hosts.txt) to app's hosts list.
 
 ## Credits
 
 * [ildoc](https://github.com/ildoc) for [HostBlock](https://github.com/ildoc/HostBlock) code base.
-* Thanks to all [maintainers](https://github.com/games195/AdHosts/blob/master/AdHosts.py#L12) of ad-hosts lists.
+* Thanks to all [maintainers](https://github.com/games195/AdHosts/blob/master/AdHosts.py#L11) of hosts lists.
 
 ## Licensing
 
-This software is licensed under MIT License.
-You can find a copy of the license in the `LICENSE.txt` file.
+This software is licensed under MIT License. You can find a copy of the license in the [`LICENSE.txt`](/LICENSE.txt) file.
