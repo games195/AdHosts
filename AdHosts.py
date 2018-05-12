fa654ff0629c1c8ab4e84537e38f052e43837d8f
Unknown <games195dev@gmail.com>
2018-05-12 15:25:22 +0200

    Add whitelist

--- AdHosts.py
+++ AdHosts.py
@@ -8,12 +8,12 @@ import urllib.request, datetime, os
 
 File = 'host/hosts.txt'
 List = []
-# Thanks to all maintainers of ad-hosts lists.
+# Thanks to all maintainers of hosts lists.
 Sources = [
+	'https://raw.githubusercontent.com/EnergizedProtection/EnergizedHosts/master/EnergizedAd/energized/hosts',
 	'https://raw.githubusercontent.com/AdroitAdorKhan/Energized/master/EnergizedHosts/EnergizedHosts',
 	'https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews-gambling/hosts',
 	'https://gist.githubusercontent.com/games195/4dced15b925a4cc28e40285b0c05ac31/raw/hosts.txt',
-	'https://raw.githubusercontent.com/EnergizedProtection/EnergizedHosts/master/EnergizedAd/energized/hosts',
 	'https://raw.githubusercontent.com/biroloter/Mobile-Ad-Hosts/master/hosts'
 ]
 
@@ -35,6 +35,17 @@ for Link in Sources:
 
 print('[+] Removing duplicates and sorting...')
 List = sorted(list(set(List)))
+print('[+] Applying whitelist...')
+r = urllib.request.urlopen('https://gist.githubusercontent.com/games195/fb3c38df42faa7468e25dc7a1c46e89e/raw/whitelist_hosts.txt')
+Whitelist = r.readlines()
+Whitelist = [x.decode('utf-8') for x in Whitelist]
+Whitelist = [x.strip() for x in Whitelist]
+Whitelist = [z for z in Whitelist if z != '' and z[0] != '#']
+r.close()
+
+for i in range(0, len(Whitelist)):
+	List.remove(Whitelist[i])
+
 print('[+] Total domains count {}.'.format(str(len(List))))
 
 if not os.path.exists(os.path.dirname(File)):
